7457f45f29b72c398277e9eaf9c574620a7edde6
dharlerjr <harler.david@gmail.com>
2024-04-16 21:41:45 -0700

    still accomodating for cdlDF global filter of maps

--- cdl-betting-app-v02/great_tables_helpers.py
+++ cdl-betting-app-v02/great_tables_helpers.py
@@ -1,17 +1,25 @@
 
-# Import pandas 
+# Import pandas & numpy
 import pandas as pd
+import numpy as np
 
 # Import great_tables 
 from great_tables import *
-
-# Import filter_maps from setup.py
-from setup.setup import filter_maps
 
 # Function to build team summary gtTable
 def team_summaries_gt_fn(
         team_summaries_input: pd.DataFrame, team_x: str, team_y: str
     ):
+
+    # In build_team_summaries from setup, we replaced all NA values with 0s. 
+    # Here, we will put those NA values back in for styling purposes
+
+    # Identify rows where both 'win' and 'loss' are 0
+    mask = (team_summaries_input['wins'] == 0) & (team_summaries_input['losses'] == 0)
+
+    # Replace win_percentage with NaN where the condition is true
+    team_summaries_input.loc[mask, 'win_percentage'] = np.nan
+
     gt_df = \
         pd.merge(
             team_summaries_input[team_summaries_input["team"] == team_x], 
@@ -93,7 +101,7 @@ def h2h_summary_gt_fn(cdlDF_input, team_x: str, team_y: str):
     h2h_summary_DF_bottom.insert(position, "map_name", h2h_summary_DF_bottom.pop("map_name"))
 
     # H2H Summary by Map & Mode
-    h2h_summary_DF_top = filter_maps(cdlDF_input) \
+    h2h_summary_DF_top = cdlDF_input \
         [["match_id", "team", "map_name", "gamemode", "map_result", "opp"]] \
         [(cdlDF_input["team"] == team_x) & \
          (cdlDF_input["opp"] == team_y)] \
